55f87239a488efe9cf4585824d440cee14a4488d
SrAbnerP <abner.sol.pereira@gmail.com>
2023-06-20 18:41:53 -0300

    add rotas autenticadas

--- minhasfinancas-app/src/main/Rotas.jsx
+++ minhasfinancas-app/src/main/Rotas.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Login from "../views/usuarios/login";
 import CadastroUsuario from "../views/usuarios/cadastro-usuario";
@@ -7,21 +7,31 @@ import Home from "../views/home";
 import ConsultaLancamento from "../views/lancamentos/consulta-lancamentos";
 import CadastroLancamentos from "../views/lancamentos/cadastro-lancamentos";
 
+import AuthService from "../app/service/authService";
+
 export default function Rotas(props) {
+  const PrivateRoute = ({ path, element }) => {
+    if (AuthService.isUsuarioAutenticado()) {
+      return element;
+    } else {
+      return <Navigate to="/login" />;
+    }
+  };
+
   return (
     <div>
       <HashRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro-usuarios" element={<CadastroUsuario />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<PrivateRoute element={<Home />} />} />
           <Route
             path="/consulta-lancamentos"
-            element={<ConsultaLancamento />}
+            element={<PrivateRoute element={<ConsultaLancamento />} />}
           />
           <Route
             path="/cadastro-lancamentos/:id?"
-            element={<CadastroLancamentos />}
+            element={<PrivateRoute element={<CadastroLancamentos />} />}
           />
         </Routes>
       </HashRouter>

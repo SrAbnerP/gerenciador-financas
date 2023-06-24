42b5add451c7a7faf091956814a7f8dc7bb4a2d7
SrAbnerP <abner.sol.pereira@gmail.com>
2023-06-23 22:45:08 -0300

    add liberação de rotas para o front end react

--- minhasfinancas-app/src/index.js
+++ minhasfinancas-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./main/App";
+import App from "./controller/App";
 import AuthProvider from "./app/provider/authProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
